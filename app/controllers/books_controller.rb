9f114bbf9254378de799634740d1fd0e1e7d7560
s-keito <110315888+s-keito@users.noreply.github.com>
2022-08-08 14:54:29 +0000

    [add]task2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,10 +4,15 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+     flash[:notice] = "Book was successfully created."
+     redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
+  
   
 
   def index
@@ -19,10 +24,30 @@ class BooksController < ApplicationController
   end
 
   def edit
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to books_path(bokk.id)
+    @book = Book.find(params[:id])
   end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.save
+     flash[:notice] = "Book was successfully updated."
+     redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+  
+  
+  
+  
+  
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   
   private
   def book_params
