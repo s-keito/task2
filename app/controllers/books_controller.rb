45456296985c79c79613f849dcc0090c9b433f77
s-keito <110315888+s-keito@users.noreply.github.com>
2022-08-08 10:05:03 +0000

    [state]task2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,32 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+  
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to books_path(bokk.id)
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
+
